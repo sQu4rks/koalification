1a4cdb0a36d2cd6d0b847148fe7e73fa58f952d4
Marcel Neidinger <neidinger@phaenovum.de>
2015-10-27 14:23:01 +0100

    Fucked up the autoload bootstrap

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,3 +1,4 @@
 <?php
-  require_once(dirname(__FILE__)."/../src/db/*.php");
+  require_once(dirname(__FILE__)."/../src/db/OrderFilter.class.php");
+  require_once(dirname(__FILE__)."/../src/db/QueryFilter.class.php");
 ?>
